7c5b9d9140eff9f7e637f4b038a7a53a7fb997a7
Cassio Ludewigs <ludewigs@gmail.com>
2016-03-28 01:57:20 +0200

    fixes to backend

--- backend/contatosBackend.js
+++ backend/contatosBackend.js
@@ -1,48 +1,48 @@
-var express = require('express');
-var app = express();
-var ii = "";
+var express = require('express')
+var app = express()
+var path = require('path')
 
-app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.bodyParser())
 
 var contatos = [
-	{id: 1, nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
-	{id: 2, nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
-	{id: 3, nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
-];
+  {id: 1, nome: 'Bruno', telefone: '9999-2222', data: new Date(), operadora: {nome: 'Oi', codigo: 14, categoria: 'Celular'}},
+  {id: 2, nome: 'Sandra', telefone: '9999-3333', data: new Date(), operadora: {nome: 'Vivo', codigo: 15, categoria: 'Celular'}},
+  {id: 3, nome: 'Mariana', telefone: '9999-9999', data: new Date(), operadora: {nome: 'Tim', codigo: 41, categoria: 'Celular'}}
+]
 var operadoras = [
-	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
-	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
-	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
-	{nome: "GVT", codigo: 25, categoria: "Fixo", preco: 1},
-	{nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
-];
+  {nome: 'Oi', codigo: 14, categoria: 'Celular', preco: 2},
+  {nome: 'Vivo', codigo: 15, categoria: 'Celular', preco: 1},
+  {nome: 'Tim', codigo: 41, categoria: 'Celular', preco: 3},
+  {nome: 'GVT', codigo: 25, categoria: 'Fixo', preco: 1},
+  {nome: 'Embratel', codigo: 21, categoria: 'Fixo', preco: 2}
+]
 
-app.listen(process.env.PORT || 3412);
+app.listen(process.env.PORT || 3412)
 
-app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.get('/contatos', function (req, res) {
-  res.json(contatos);
-});
+  res.json(contatos)
+})
 
 app.get('/contatos/:id', function (req, res) {
-  contatos.forEach(function(contato) {
-    if (req.params.id == contato.id)
-      res.json(contatos[req.params.id-1]);
-  });
-});
+  contatos.forEach(function (contato) {
+    if (req.params.id === contato.id)
+      res.json(contatos[req.params.id-1])
+  })
+})
 
 app.post('/contatos', function (req, res) {
-  contatos.push(req.body);
-  res.json(true);
-});
+  contatos.push(req.body)
+  res.json(true)
+})
 
-app.get('/operadoras', function(req, res) {
-  res.json(operadoras);
-});
+app.get('/operadoras', function (req, res) {
+  res.json(operadoras)
+})
